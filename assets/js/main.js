79e5f0d0e4d4d542ea3dbd4f2336fa5c0205a1d6
RP-Developer <98476877+RP-developer-01@users.noreply.github.com>
2023-02-13 22:03:06 +0200

    fix bug basket

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,63 @@
+function stepper(btn, idproduct) {
+  //const countField = document.querySelector('.count__input'); тут ти вибирав перший елемент який має класс .count__input
+  let countField = document.querySelector(`#count__input${idproduct}`); //тут вибирається різні елемент з різними id, якшо коротко при нажатиии на кнопку передаються ід товара яке підставляється сюди аби знати з яким товаром проходться зміни
 
+  let id = btn.getAttribute("id");
+  let min = countField.getAttribute("min");
+  let max = countField.getAttribute("max");
+  let step = countField.getAttribute("step");
+  let val = countField.getAttribute("value");
 
-function stepper(btn){
-    const countField = document.querySelector('.count__input');
-    let id = btn.getAttribute("id");
-    let min = countField.getAttribute("min");
-    let max = countField.getAttribute("max");
-    let step = countField.getAttribute("step");
-    let val = countField.getAttribute("value");
+  let calcStep = +(id == "increment") ? step * +1 : step * -1;
+  let newValue = parseInt(val) + calcStep;
 
-    let calcStep = +(id == "increment") ? (step*+1) : (step* -1);
-    let newValue = parseInt(val) + calcStep;
+  console.log(newValue);
 
-    console.log(newValue);
+  if (newValue >= min && newValue <= max) {
+    countField.setAttribute("value", newValue);
+  }
+}
 
-    if (newValue >= min && newValue <= max){
-        countField.setAttribute("value", newValue);
-    }
+function addToCart(id) {
+  $.ajax({
+    type: "POST",
+    url: "/controllers/CartController.php",
+    data: { id: id },
+    dataType: "html",
+    success: function (data) {
+      //if(data['success']) {
+      $("#addToCart_" + id).html("Added to cart");
+      $("#count_products_cart").html(data);
+      console.log(data);
+      // }
+    },
+  });
+}
+
+async function deleteProduct(id) {
+  $.ajax({
+    url: "/controllers/DeleteCartController.php",
+    type: "post",
+    data: { id: id },
+  })
+    .done(function (result) {
+      //$('#product' + id).remove(result);
+      //$('#restart').html(result);
+      //$('#product' + id).html('');
+      $("#count_products_cart").html(result);
+      
+      //location.reload();
+    })
+    .fail(function () {
+      console.log("fail");
+    });
+
+    updateShopCart();
 }
 
 
-    function addToCart(id){
+/*===============================*/
 
-        $.ajax({
-            type: "POST",
-            url:"/controllers/CartController.php",
-            data: {id: id},
-            dataType: 'html',
-            success:( function (data){
-                //if(data['success']) {
-                    $('#addToCart_' + id).html("Added to cart");
-                    $('#count_products_cart').html(data);
-                    console.log(data);
-               // }
-            })
-        })
-    }
-
-
-    async function deleteProduct(id){
-            $.ajax({
-                url:"/controllers/DeleteCartController.php",
-                type: "post",
-                data: {id: id},
-                }).done(function (result) {
-                 //$('#product' + id).remove(result);
-                // $('#restart').html(result);
-                //$('#product' + id).html('');
-                $('#count_products_cart').html(result);
-                   //location.reload();
-                }).fail(function () {
-                    console.log('fail');
-            });
-    }
-
-
+function updateShopCart() {
+    $("#count_products_cart").html(data);
+}
--- config/db.php
+++ config/db.php
@@ -1,17 +1,17 @@
 <?php
-//$servername = "localhost";
-//$database = "seedra";
-//$username = "root";
-//$password = "";
-//
-//$conn = mysqli_connect($servername, $username, $password, $database);
-//
-//$conn->set_charset("utf8");
-//
-//if(!$conn){
-//    die("Conection failed: " . mysqli_connect_error());
-//}
-////echo "Connected successfully!";
+$servername = "localhost";
+$database = "seedra";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+$conn->set_charset("utf8");
+
+if(!$conn){
+    die("Conection failed: " . mysqli_connect_error());
+}
+//echo "Connected successfully!";
 
 $db = new PDO('mysql:host=localhost; dbname=seedra', 'root', '');
 
